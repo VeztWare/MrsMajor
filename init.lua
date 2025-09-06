e41ba6fa21c8a009994049c598f72cf607210cd6
VeztWare <117451997+VeztWare@users.noreply.github.com>
2025-09-06 23:15:58 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,1 +1,1 @@
-loadstring(readfile("https://raw.githubusercontent.com/VeztWare/MrsMajor/refs/heads/main/loader.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/VeztWare/MrsMajor/refs/heads/main/loader.lua"))()
